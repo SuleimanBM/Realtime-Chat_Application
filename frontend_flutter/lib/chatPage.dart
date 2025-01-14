f3a7d1c22fe03d0b0124ac03ba5d82c865230fd6
Suleiman-baba <bmdanjumah@gmail.com>
2025-01-14 22:44:53 +0000

    A little improvement today

--- frontend_flutter/lib/chatPage.dart
+++ frontend_flutter/lib/chatPage.dart
@@ -16,45 +16,62 @@ class _ChatpageState extends State<Chatpage> {
       appBar: AppBar(
         title: Text("Chat"),
       ),
-      body: SingleChildScrollView(
-        child: Column(children: [
-          Container(
-            //height: 200,
-            width: 300,
-            padding: EdgeInsets.all(5),
-            margin: EdgeInsets.only(left: 5),
-            decoration: BoxDecoration(
-              color: Colors.red[900],
-              borderRadius: BorderRadius.circular(7),
-            ),
-            child: Text(
-              "Chat",
-              style: TextStyle(color: Colors.white, fontSize: 18),
-            ),
-          ),
-          
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              padding: EdgeInsets.only(left: 10, right: 2),
-              decoration: BoxDecoration(
-                border: Border.all(width: 2.0),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(border: InputBorder.none),
-                    ),
+      body: Stack(children: [
+        SingleChildScrollView(
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  //height: 200,
+                  width: 300,
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 7),
+                  margin: EdgeInsets.only(left: 5),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 218, 216, 216),
+                    borderRadius: BorderRadius.circular(7),
                   ),
-                  FloatingActionButton(mini: true,onPressed: (){}, child: Container(child: Icon(Icons.send)),)
-                ],
-              ),
+                  child: Text(
+                    "Chatchatchatchatchatchatchatchatchatchatchatchat",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        )
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            padding: EdgeInsets.only(left: 10, right: 2),
+            margin: EdgeInsets.only(bottom: 5,left: 5, right: 5),
+            decoration: BoxDecoration(
+              border: Border.all(width: 2.0),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(border: InputBorder.none),
+                  ),
+                ),
+                FloatingActionButton(
+                  mini: true,
+                  onPressed: () {},
+                  child: Container(child: Icon(Icons.send)),
+                )
+              ],
             ),
           ),
-        ]),
-      ),
+        ),
+      ]),
     );
   }
 }
